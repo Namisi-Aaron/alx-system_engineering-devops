2c6c3eb8f197a34ec14d9eea6c1fd89a1673d931
Namisi-Aaron <aaronnamisi@gmail.com>
2024-07-02 22:24:11 -0700

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
 # This manifest installs flask version 2.1.0
+package { 'Python':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
